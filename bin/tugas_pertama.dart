9d5157a74adb536556ceeb6c8d88540a3e463aa7
Jafar Muhammad <jafarmuhh24@gmail.com>
2025-08-26 15:48:19 +0900

    first commit

---
+++ bin/tugas_pertama.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_pertama/tugas_pertama.dart' as tugas_pertama;
+
+void main(List<String> arguments) {
+  print('Hello Pemrograman Mobile 5IF1: ${tugas_pertama.calculate()}!');
+}
